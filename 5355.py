9482328a0447ad1b6fbeafc83ee737e035583bed
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-01-22 22:12:54 +0900

    update

--- 5355.py
+++ 5355.py
@@ -1,1 +1,18 @@
-print("HelloHyojeon")
+import sys
+sys.stdin = open("input.txt", 'r')
+
+n = input()
+
+for i in range(int(n)):
+    str = input().split()
+    tmp = float(str[0])
+    for j in range(1, len(str)):
+        if str[j] == '@':
+            tmp *= 3
+        elif str[j] == '%':
+            tmp += 5
+        elif str[j] == '#':
+            tmp -= 7
+    print(f'{tmp:.2f}')
+
+
