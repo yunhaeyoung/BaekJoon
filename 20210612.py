4155058662b43da8565ce15dd35e840e21de026e
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-06-12 18:37:05 +0900

    Create 20210612.py

---
+++ 20210612.py
@@ -0,0 +1,94 @@
+#숫자 출력 
+# print(5)
+# print(-10)
+# print(3.14)
+# print(5+3)
+# print(8*8)
+# print(3*(3+1))
+
+#문자열 출력
+# print('작은 따옴표')
+# print("큰 따옴표")
+# print("오에"*10)
+
+#boolean
+# print(5 > 10)
+# print(5 < 10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not (5 > 3))
+
+#변수
+# animal = "고양이"
+# name = "땅콩스"
+# age = 1.5
+# hobby = "사냥"
+# is_adult = age >= 1
+
+# print(animal + "이름은" + name)
+# print(name + "는 " + str(age) + "살이고 " + hobby + "를 매우 좋아함")
+# print(name, "는 ", age, "살이고 ", hobby, "를 매우 좋아함")
+# print(name + "는 성묘냐? " + str(is_adult))
+
+ #str()함수를 사용하지 않는 방법 -> , 쓰기!
+#문자열을 제외하고는 모두 str으로 바꿔줘야함
+
+#주석
+'''나아아아아
+아아아아
+아아아아
+아아아아'''
+#여러 줄 쓰기 -> 작은 따옴표 세번!!!
+
+#Quiz 1
+# station = "사당"
+# print(station + "행 열차가 들어오고 있습니다.")
+# station = "신도림"
+# print(station + "행 열차가 들어오고 있습니다.")
+# station = "인천공항"
+# print(station + "행 열차가 들어오고 있습니다.")
+
+#연산자
+# print(1+1)
+# print(4-1)
+# print(5*8)
+# print(8/4)
+
+# print(2**3) #2^3
+# print(5%4) #나머지
+# print(5//4) #몫
+
+# print(10 > 5)
+# print(5 <= 7)
+# print(10 < 2)
+
+# print(1 == 1)
+# print(5 + 4 == 9)
+# print(5 != 9)
+# print(not (5 == 9))
+
+# print((3 > 0) and (3 < 5))
+# print((3 < 0) and (3 < 5))
+# print((3 > 0) & (3 < 5))
+
+# print((3 > 0) or (3 > 5))
+# print((3 > 0) | (3 > 5))
+
+# print(5 > 4 > 3)
+# print(5 > 4 > 9)
+
+#수식
+# print(2 + 3 * 4)
+# print((2 + 3) * 4)
+# number = 2 + 3 * 4
+# print(number)
+# number += 2
+# print(number)
+# number -= 10
+# print(number)
+
+# number %= 4
+# print(number)
+
+#36:48 ~ 
