ccadf28088b4a3a4b839a9d41f917f071747fa9b
HaeYoung <37984690+yunhaeyoung@users.noreply.github.com>
2021-06-13 23:53:15 +0900

    2021/06/13

--- 1837.py
+++ 1837.py
@@ -2,12 +2,18 @@ from math import *
 
 P, K = map(int, input().split())
 
-for i in range(5, int(sqrt(P)) + 1, 2):
-    if P % i == 0:
-        p = i
-        q = P // i
+list = [1] * K
+print(list)
+# 소수 판별
+# for i in range(5, int(sqrt(P)) + 1, 2):
+#     if P % i == 0:
+#         p = i
+#         q = P // i
 
-if (p > K) & (q > K):
-    print("GOOD")
-else:
-    print("BAD", min(p, q))
+# 에라토스테네스의 체
+
+
+# if (p > K) & (q > K):
+#     print("GOOD")
+# else:
+#     print("BAD", min(p, q))
